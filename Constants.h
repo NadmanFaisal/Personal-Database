5c3f4224335f10bccf8ab84bf82ea7570ed61327
Nadman Faisal <nadman@chalmers.se>
2025-03-14 11:22:28 +0100

    Move constants from Row.h to Constants.h

--- Constants.h
+++ Constants.h
@@ -3,7 +3,24 @@
 
 #include <stdint.h>
 
-#include "Row.h"
+// Constants for Row class
+
+#define COLUMN_EMAIL_SIZE 255
+#define COLUMN_USERNAME_SIZE 32
+
+// // Accessing member variable without actually creating the variable from the struct
+// #define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
+
+static const uint32_t ID_SIZE = sizeof(uint32_t);
+static const uint32_t USERNAME_SIZE = COLUMN_USERNAME_SIZE + 1;
+static const uint32_t EMAIL_SIZE = COLUMN_EMAIL_SIZE + 1;
+
+static const uint32_t ID_OFFSET = 0;
+static const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
+static const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
+static const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
+
+// Constants for table class
 
 #define TABLE_MAX_PAGES 100
 
--- Row.h
+++ Row.h
@@ -6,28 +6,13 @@
 #include <stdint.h>
 #include <string.h>
 
-#define COLUMN_EMAIL_SIZE 255
-#define COLUMN_USERNAME_SIZE 32
-
-// Accessing member variable without actually creating the variable from the struct
-#define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
+#include "Constants.h"
 
 typedef struct {
     uint32_t id;
     char username[COLUMN_USERNAME_SIZE + 1];
     char email[COLUMN_EMAIL_SIZE + 1];
 } ROW;
-
-
-static const uint32_t ID_SIZE = size_of_attribute(ROW, id);
-static const uint32_t USERNAME_SIZE = size_of_attribute(ROW, username);
-static const uint32_t EMAIL_SIZE = size_of_attribute(ROW, email);
-
-static const uint32_t ID_OFFSET = 0;
-static const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
-static const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
-static const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
-
 
 void serialize_row(ROW *row, void *destination);
 void deserialize_row(void *source, ROW *destination);
