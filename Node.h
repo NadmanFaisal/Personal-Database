fb4c008d4893b6b7ef0e134319ba777b916b1cd4
Nadman Faisal <nadman@chalmers.se>
2025-04-03 12:34:18 +0200

    Create constants for nodes

---
+++ Node.h
@@ -0,0 +1,43 @@
+#ifndef NODE_H
+#define NODE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#include "Constants.h"
+
+typedef enum {
+    NODE_INTERNAL,
+    NODE_LEAF
+} NodeType;
+
+// Common node header layout for B-trees
+
+static const uint32_t NODE_TYPE_SIZE = sizeof(uint8_t);
+static const uint32_t NODE_TYPE_OFFSET = 0;
+static const uint32_t IS_ROOT_SIZE = sizeof(uint8_t);
+static const uint32_t IS_ROOT_OFFSET = NODE_TYPE_SIZE;
+static const uint32_t PARENT_POINTER_SIZE = sizeof(uint32_t);
+static const uint32_t PARENT_POINTER_OFFSET = IS_ROOT_OFFSET + IS_ROOT_SIZE;
+static const uint32_t COMMON_NODE_HEADER_SIZE = NODE_TYPE_SIZE + IS_ROOT_SIZE + PARENT_POINTER_SIZE;
+
+// Common leaf node header layout for B-trees
+
+static const uint32_t LEAF_NODE_NUM_CELLS_SIZE = sizeof(uint32_t);
+static const uint32_t LEAF_NODE_NUM_CELLS_OFFSET = COMMON_NODE_HEADER_SIZE;
+static const uint32_t LEAF_NODE_HEADER_SIZE = COMMON_NODE_HEADER_SIZE + LEAF_NODE_NUM_CELLS_SIZE;
+
+// Leaf node body layout
+
+static const uint32_t LEAF_NODE_KEY_SIZE = sizeof(uint32_t);
+static const uint32_t LEAF_NODE_KEY_OFFSET = 0;
+static const uint32_t LEAF_NODE_VALUE_SIZE = ROW_SIZE;
+static const uint32_t LEAF_NODE_VALUE_OFFSET = LEAF_NODE_KEY_OFFSET + LEAF_NODE_KEY_SIZE;
+static const uint32_t  LEAF_NODE_CELL_SIZE = LEAF_NODE_KEY_SIZE + LEAF_NODE_VALUE_SIZE;
+static const uint32_t LEAF_NODE_SPACE_FOR_CELLS = PAGE_SIZE - LEAF_NODE_HEADER_SIZE;
+static const uint32_t LEAF_NODE_MAX_CELLS = LEAF_NODE_SPACE_FOR_CELLS / LEAF_NODE_CELL_SIZE;
+
+
+
+#endif
