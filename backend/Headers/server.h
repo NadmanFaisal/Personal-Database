dcef2d3ebe527b4b12fcfc649ba85d70badf6dd3
Nadman Faisal <nadman@chalmers.se>
2025-04-12 17:26:58 +0200

    Create a server header file

---
+++ backend/Headers/server.h
@@ -0,0 +1,25 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+#include <netinet/in.h>
+
+#define BUFFER_SIZE 16000
+
+struct Server {
+    int domain;
+    int port;
+    int service;
+    int protocol;
+    int backlog;
+    u_long interface;
+
+    int socket;
+    struct sockaddr_in addr;
+
+    void(*launch)(struct Server *server);
+};
+
+struct Server serverConstructor(int domain, int port, int service, int protocol, int backlog, u_long interface, void(*launch)(struct Server *server));
+void launch(struct Server *server);
+
+#endif SERVER_H
